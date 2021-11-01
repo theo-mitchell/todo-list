450e7eb8047045d11f32b26f811610c10d7937e4
theom95 <theodore.mitchell95@gmail.com>
2021-11-01 17:05:40 -0500

    Initial commit. Webpack/app skeleton setup

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('meow meow meow meow');
+console.log('webpack watch is working!!');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EsMEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ21lb3cgbWVvdyBtZW93IG1lb3cnKTtcbmNvbnNvbGUubG9nKCd3ZWJwYWNrIHdhdGNoIGlzIHdvcmtpbmchIScpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log('meow meow meow meow');
+console.log('webpack watch is working!!');
